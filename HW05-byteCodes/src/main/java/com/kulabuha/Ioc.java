9192f5bebbaa9f0ec12545a6f606042d25633459
Aleksei Kulabuhov <kulabuha@gmail.com>
2022-11-14 21:50:21 +0200

    resolve comments

--- HW05-byteCodes/src/main/java/com/kulabuha/Ioc.java
+++ HW05-byteCodes/src/main/java/com/kulabuha/Ioc.java
@@ -10,22 +10,23 @@ class Ioc {
     private Ioc() {
     }
 
-    static TestLoggingInterface createMyClass() {
-        InvocationHandler handler = new DemoInvocationHandler(new TestLogging());
+    static <T extends TestLoggingInterface> TestLoggingInterface createMyClass(T t) {
+        InvocationHandler handler = new DemoInvocationHandler(t);
         return (TestLoggingInterface) Proxy.newProxyInstance(Ioc.class.getClassLoader(),
                 new Class<?>[]{TestLoggingInterface.class}, handler);
     }
 
     static class DemoInvocationHandler implements InvocationHandler {
         private final TestLoggingInterface myClass;
+        private final Class<?> classString;
 
         DemoInvocationHandler(TestLoggingInterface myClass) {
             this.myClass = myClass;
+            this.classString = myClass.getClass();
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            Class<?> classString = myClass.getClass();
             if (classString.getMethod(method.getName(), method.getParameterTypes()).isAnnotationPresent(Log.class)) {
                 System.out.println("executed method: " + method.getName() + ", param: " + Arrays.toString(args));
             }
--- HW05-byteCodes/src/main/java/com/kulabuha/Main.java
+++ HW05-byteCodes/src/main/java/com/kulabuha/Main.java
@@ -2,7 +2,7 @@ package com.kulabuha;
 
 public class Main {
     public static void main(String[] args) {
-        TestLoggingInterface myClass = Ioc.createMyClass();
+        TestLoggingInterface myClass = Ioc.createMyClass(new TestLogging());
         myClass.calculation(6, "fdsf");
         myClass.calculation(6);
     }
